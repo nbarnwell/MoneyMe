6f8129eb4bb1d2306be306d60b9662787d2809b4
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2015-09-05 20:00:09 +0100

    Project created.

---
+++ MoneyMe.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoneyMe.Web.Startup))]
+namespace MoneyMe.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
