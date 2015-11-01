8ecc2fe5b246b6655d6baf0d00917639ed3cb660
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2015-11-01 22:46:44 +0000

    Added controller for listing model types for scaffolding.

---
+++ MoneyMe.Web/Controllers/ModelScaffoldingController.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using MoneyMe.Web.Models.Scaffolding;
+
+namespace MoneyMe.Web.Controllers
+{
+    public class ModelScaffoldingController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var types =
+                typeof(IScaffoldDbContext).GetProperties()
+                                          .Select(x => x.PropertyType)
+                                          .Where(x => x.IsGenericType)
+                                          .Where(x => typeof(IDbSet<>).IsAssignableFrom(x.GetGenericTypeDefinition()))
+                                          .Select(x => x.GetGenericArguments().First().Name);
+
+            var content = string.Join("\r\n", types.Select(BuildScaffoldString).ToArray());
+            return new FileContentResult(Encoding.UTF8.GetBytes(content), "text/plain");
+        }
+
+        private string BuildScaffoldString(string typeName)
+        {
+            return string.Format("Scaffold Controller -Area 'Setup' -ModelType '{0}' -ControllerName '{0}Controller' -DbContextType 'ScaffoldDbContext'", typeName);
+        }
+    }
+}
